5027be70b8f019c9423c686379748804a35c3a97
goldendevuz <goldendevuz@gmail.com>
2025-11-25 00:32:06 +0500

    2025.11.25/00:32

--- library/management/commands/import_pdf_books.py
+++ library/management/commands/import_pdf_books.py
@@ -1,16 +1,43 @@
+import logging
 from django.core.management.base import BaseCommand
 
-from library.models import BookModel
+from library.models import BookModel        
+from pathlib import Path
+
+
+import os
 
 class Command(BaseCommand):
     help = 'Generate 1 identical book entries for testing'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--d',
+            type=str,
+            help='The path to the directory to process.',
+            required=True  # Make the argument mandatory
+        )
+
+
     def handle(self, *args, **kwargs):
+        # cwd = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+        # files = os.listdir(cwd)  # Get all the files in that directory
+        # print("Files in %r: %s" % (cwd, files))
+        # print(kwargs.items())
+        directory_path = Path(kwargs['d'])
+        # directory_path = 'media/book/file/'  # Represents the current directory
+
+        print(directory_path, "d")
+        # print([a for a in os.walk(directory_path)])
+        # # files_list = [p for p in os.scandir(directory_path) if p.endswith('.pdf')]
+        files_list = [p for p in directory_path.iterdir() if p.suffix == '.pdf']
+        # print(files_list, "files_list")
         books = []
-        for i in range(1000):
+        for file_path in files_list:
             books.append(
                 BookModel(
-                    file="Ch Aytmatov O tar qush nolasi (1).pdf",
+                    file=str(file_path).replace(str(directory_path), ''),
+                    title=str(file_path.name).replace('.pdf', ''),
                 )
             )
 
